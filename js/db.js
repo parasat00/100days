232d82f0e75bf8e4977aa7e0ab2e5637f092a32e
Parasat Zhangbyrbay <63976887+parasat00@users.noreply.github.com>
2023-06-01 00:05:52 +0600

    second

--- js/db.js
+++ js/db.js
@@ -6,11 +6,4 @@ export const db = [
    thoughts: "Feeling full of hope",
    link: "https://github.com/parasat00/SDU-Career-Roadmap"
  },
- {
-   dayNumber : 1,
-   dayStr : "May 31. Wednesday",
-   description: "Spent time working on Ecommerce website, created this page",
-   thoughts: "Feeling full of hope",
-   link: "https://github.com/parasat00/SDU-Career-Roadmap"
- },
 ];
