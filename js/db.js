5ca6f2b41726610667db0790c7f39d6cad36554f
Parasat Zhangbyrbay <63976887+parasat00@users.noreply.github.com>
2023-06-01 20:40:48 +0600

    day2

--- js/db.js
+++ js/db.js
@@ -4,6 +4,13 @@ export const db = [
    dayStr : "May 31. Wednesday",
    description: "Spent time working on Ecommerce website, created this page",
    thoughts: "Feeling full of hope",
-   link: "https://github.com/parasat00/SDU-Career-Roadmap"
+  //  link: "https://github.com/parasat00/SDU-Career-Roadmap"
+ },
+ {
+   dayNumber : 2,
+   dayStr : "June 1. Thursday",
+   description: "Worked more on Ecommerce website",
+   thoughts: "Feeled down because understood that I have a lot of things to learn. It is important to not give up",
+  //  link: "https://github.com/parasat00/SDU-Career-Roadmap"
  },
 ];
